32a7dc31769a678bee2fe81af679c5bc8f51ba46
trevorkloz <trevor@kloz.net>
2026-01-12 19:29:24 +0100

    Add possibility to generate numbers, some code cleanup, some comments

--- pyDominoPDF.py
+++ pyDominoPDF.py
@@ -1,22 +1,44 @@
-__license__ = 'GNU Public License Version 3, https://www.gnu.org/licenses/gpl-3.0.en.html'
-__copyright__ = "Copyright (C) 2020, Dev@BernCodes.com - Released under terms of the GPLv3 License"
-
 from bitstring import BitArray
 from random import shuffle
 from pyDominoValueGenerator import Valid_Dominos
 from datetime import datetime
 from math import floor
-import pyx
 import logging
+import sys
+from fpdf import FPDF
+
+# Domino colors
+DOMINO_FOREGROUND = (255, 255, 255)  # white
+DOMINO_BACKGROUND = (0, 0, 0)  # black
 
 
-class Margin():
+def draw_rounded_rect(pdf, x, y, w, h, r):
+    """
+    Draw a rectangle with rounded corners.
+    x, y: top-left corner
+    w, h: width and height
+    r: corner radius
+    """
+    pdf.set_fill_color(*DOMINO_BACKGROUND)
+    # Draw four corner circles (quarter circles)
+    pdf.ellipse(x, y, 2 * r, 2 * r, style='F')  # Top-left
+    pdf.ellipse(x + w - 2 * r, y, 2 * r, 2 * r, style='F')  # Top-right
+    pdf.ellipse(x, y + h - 2 * r, 2 * r, 2 * r, style='F')  # Bottom-left
+    pdf.ellipse(x + w - 2 * r, y + h - 2 * r, 2 * r, 2 * r, style='F')  # Bottom-right
+
+    # Draw rectangles to fill sides
+    pdf.rect(x + r, y, w - 2 * r, h, style='F')  # horizontal central rect
+    pdf.rect(x, y + r, w, h - 2 * r, style='F')  # vertical central rect
+
+
+class Margin:
     Top = 0.0
     Left = 0.0
     Right = 0.0
     Bottom = 0.0
 
-class Paper():
+
+class Paper:
     Height = 0.0
     Width = 0.0
     Margin = Margin()
@@ -26,7 +48,8 @@ class Paper():
     X_ScaleFactor = 1.0
     Y_ScaleFactor = 1.0
 
-class pyDominoPDF:
+
+class DominoPDF:
     MarginBorder = False
 
     RowSpacing = 3.0
@@ -34,256 +57,156 @@ class pyDominoPDF:
     VerificationScale = False
     Randomize = True
     PrintValues = False
-    MarginBorder = False
     DebugFile = True
     RadiusCorners = False
 
-    #__pyx_d = None
     _units = "inch"
     _unit_scale = 1.0
 
     @property
     def Units(self):
-        #Unit Property Get
         return self._units
 
     @Units.setter
     def Units(self, value):
-        #Unit Property Set
-        if (value == "inch"):
+        if value == "inch":
             self._unit_scale = 1.0
-        elif (value == "mm"):
+        elif value == "mm":
             self._unit_scale = 25.4
-        elif (value == "cm"):
+        elif value == "cm":
             self._unit_scale = 2.54
         else:
             self._unit_scale = 1.0
-
         self._units = value
-    
+
     @property
     def DominoPadding(self):
-        return (0.125 * self._unit_scale)
+        return 0.125 * self._unit_scale
 
     @property
     def DominoRadius(self):
-        return (.0625 * self._unit_scale)
-    
+        return 0.0625 * self._unit_scale
+
     @property
     def Pip_Padding(self):
-        return (0.10 * self._unit_scale)
-    
+        return 0.10 * self._unit_scale
+
     @property
     def Domino_Width(self):
-        return (1.7 * self._unit_scale)
-    
+        return 1.7 * self._unit_scale
+
     @property
     def Domino_Height(self):
-        return (0.5 * self._unit_scale)
+        return 0.5 * self._unit_scale
+
     @property
     def CornerRadius(self):
-        return (0.10 * self._unit_scale)
-    
+        return 0.10 * self._unit_scale
+
     @property
     def Pip_Diameter(self):
-        return (0.10 * self._unit_scale)
+        return 0.10 * self._unit_scale
 
     @property
     def Pip_Radius(self):
-        return (0.05 * self._unit_scale)
+        return 0.05 * self._unit_scale
 
-    def __place_domino(self, x,y, value, canvas):
-        #Paints Domino, X,Y @ Left Corner of Domino
-
-        #PYX Library
-        if (self.RadiusCorners == True):
-            w = self.Domino_Width
-            h = self.Domino_Height
-            r = self.CornerRadius
-
-            pyx_circle = pyx.path.circle(x+r, y+r, r)
-            canvas.fill(pyx_circle, [pyx.color.rgb.black])
-
-            pyx_circle = pyx.path.circle((x+w)-r, y+r, r)
-            canvas.fill(pyx_circle, [pyx.color.rgb.black])
-
-            pyx_circle = pyx.path.circle(x+r, (y+h)-r, r)
-            canvas.fill(pyx_circle, [pyx.color.rgb.black])
-
-            pyx_circle = pyx.path.circle((x+w)-r, (y+h)-r, r)
-            canvas.fill(pyx_circle, [pyx.color.rgb.black])
-
-            pyx_rec = pyx.path.rect (x, y+r, w, h-(2*r))
-            canvas.fill(pyx_rec, [pyx.color.rgb.black])
-
-            pyx_rec = pyx.path.rect (x+r, y, w-(2*r), h)
-            canvas.fill(pyx_rec, [pyx.color.rgb.black])
-
+    def __place_domino(self, pdf: FPDF, x, y, value):
+        # Draw domino background
+        pdf.set_fill_color(*DOMINO_BACKGROUND)
+        if self.RadiusCorners:
+            draw_rounded_rect(pdf, x, y, self.Domino_Width, self.Domino_Height, self.CornerRadius)
         else:
-            pyx_rec = pyx.path.rect (x, y, self.Domino_Width, self.Domino_Height)
-            canvas.fill(pyx_rec, [pyx.color.rgb.black])
+            pdf.rect(x, y, self.Domino_Width, self.Domino_Height, style='F')
 
+        self.drawValueText(pdf, value, x, y, self.Domino_Width, self.Domino_Height)
 
-        px = 0
-        py = 0
-
-        DPips = BitArray('0x000')
-        DPips.uint = int(value)
-
-        #BitArrays are big endian by default
-        BitRow = [BitArray(6), BitArray(6)]
-
-        BitRow[0] = DPips[:6] # First Row
-        BitRow[1]= DPips[-6:] # Second Row
-        
+        # Draw pips
+        DPips = BitArray(uint=int(value), length=12)
+        BitRow = [DPips[:6], DPips[6:]]  # two rows
         for py in range(2):
             for px in range(8):
-                #First and list pip are always placed
-                #Pip 1-6 based on bitrow
-                if (px == 0) or (px == 7) or (BitRow[py][px-1] is True):
-                    YCord = y + (self.Pip_Padding+(self.Pip_Radius)+((py*2)*(self.Pip_Diameter)))
-                    XCord = x + (self.Pip_Padding+(self.Pip_Radius)+((px*2)*(self.Pip_Diameter)))
+                if px == 0 or px == 7 or BitRow[py][px - 1]:
+                    pip_x = x + self.Pip_Padding + self.Pip_Radius + (px * 2 * self.Pip_Diameter)
+                    pip_y = y + self.Pip_Padding + self.Pip_Radius + (py * 2 * self.Pip_Diameter)
+                    pdf.set_fill_color(*DOMINO_FOREGROUND)
+                    pdf.ellipse(pip_x - self.Pip_Radius, pip_y - self.Pip_Radius,
+                                self.Pip_Diameter, self.Pip_Diameter, style='F')
 
-                    pyx_circle = pyx.path.circle(XCord, YCord, self.Pip_Radius)
-                    canvas.fill(pyx_circle, [pyx.color.rgb.white])
+    def drawValueText(self, pdf, value, x, y, w, h):
+        if self.PrintValues:
+            # optional label in corner
+            pdf.set_text_color(0, 0, 0)
+            pdf.set_font("Courier", "", 6)
+            pdf.text(x + 1.45, y + 0.60, "{:04d}".format(value))
 
-        return ()
+    def __prepare_file(self, pdf: FPDF):
+        if self.Randomize:
+            shuffle(Valid_Dominos)
 
-    def __prepare_file(self, pyx_document):
+        work_width = self.Page.Width - (self.Page.Margin.Left + self.Page.Margin.Right)
+        work_height = self.Page.Height - (self.Page.Margin.Top + self.Page.Margin.Bottom)
 
-        #ValidValues = DominoValues(0,4095)
-        if (self.Randomize == True): shuffle(Valid_Dominos)
+        pg_Num_Rows = 1 + floor((work_height - self.Domino_Height) / (self.Domino_Height + self.RowSpacing))
+        pg_Num_Cols = 1 + floor((work_width - self.Domino_Width) / (self.Domino_Width + self.DominoPadding))
 
-        work_width = self.Page.Width-(self.Page.Margin.Left+self.Page.Margin.Right)
-        work_height = self.Page.Height-(self.Page.Margin.Top+self.Page.Margin.Bottom)
+        x_offset = 0
+        y_offset = 0
+        if self.Page.Center_Horizontal:
+            x_domino_space = (pg_Num_Cols * (self.Domino_Width + self.DominoPadding)) - self.DominoPadding
+            x_offset = round((work_width - x_domino_space) / 2, 4)
+        if self.Page.Center_Vertical:
+            y_domino_space = (pg_Num_Rows * (self.Domino_Height + self.RowSpacing)) - self.RowSpacing
+            y_offset = round((work_height - y_domino_space) / 2, 4)
 
-        pg_Num_Rows = 1+floor((work_height-self.Domino_Height)/(self.Domino_Height+self.RowSpacing)) # Number of Complete Rows
-        pg_Num_Cols = 1+floor((work_width-self.Domino_Width)/(self.Domino_Width+self.DominoPadding))
-
-        if (self.Page.Center_Horizontal == True):
-            x_domino_space = (pg_Num_Cols*(self.Domino_Width+self.DominoPadding))-self.DominoPadding
-            x_offset = round((work_width-x_domino_space)/2, 4)
-        else:
-            x_offset = 0
-
-        if (self.Page.Center_Vertical  == True):
-            y_domino_space =  (pg_Num_Rows*(self.Domino_Height+self.RowSpacing))-self.RowSpacing
-            y_offset = round((work_height-y_domino_space)/2, 4)
-        else:
-            y_offset = 0
-        
-        xPage = 0
         ValueIndex = 0
         ValueIndexMax = len(Valid_Dominos)
 
-        if (self.VerificationScale == True) :
-            pyx_vsc = pyx.canvas.canvas()
-            for v in range(5):
-                vsc_r =  pyx.path.rect (0+self.Page.Margin.Left, (v*self._unit_scale)+(0.5*v*self._unit_scale)+self.Page.Margin.Bottom, (5-v)*self._unit_scale, 1*self._unit_scale)
-                pyx_vsc.fill(vsc_r, [pyx.color.rgb.black])
-
-                vert_scale_x = (self.Page.Width-self.Page.Margin.Right)-(v*self._unit_scale)-(0.5*v*self._unit_scale)-(1*self._unit_scale)
-                vert_scale_y = self.Page.Height-self.Page.Margin.Top - (5-v)*self._unit_scale
-                vsc_r =  pyx.path.rect (vert_scale_x, vert_scale_y, 1*self._unit_scale, (5-v)*self._unit_scale)
-                logging.debug("width: {} height: {} at {},{}" . format(1*self._unit_scale, (5-v)*self._unit_scale, vert_scale_x, vert_scale_y))
-                pyx_vsc.fill(vsc_r, [pyx.color.rgb.black])
-
-            pyx_vsc_page = pyx.document.page(pyx_vsc,None, pyx.document.paperformat(self.Page.Width, self.Page.Height),0,0,0,0)
-            pyx_document.append(pyx_vsc_page)
-
-
-        for xPage in range(self.Page.Count):
-            logging.debug("Page {}" . format(xPage))
-            pyx_c = pyx.canvas.canvas()
-
-            if (self.MarginBorder == True):
-                #Margin Rectangle
-                mr_x = self.Page.Margin.Left
-                mr_y = self.Page.Margin.Bottom
-                mr_height = self.Page.Height-(self.Page.Margin.Top+self.Page.Margin.Bottom)
-                mr_width = self.Page.Width-(self.Page.Margin.Left+self.Page.Margin.Right)
-
-                margin_rect =  pyx.path.rect (mr_x, mr_y, mr_width,mr_height)
-                pyx_c.stroke(margin_rect, [pyx.color.rgb.blue])
+        for page in range(self.Page.Count):
+            pdf.add_page()
+            if self.MarginBorder:
+                pdf.set_draw_color(0, 0, 255)
+                pdf.rect(self.Page.Margin.Left, self.Page.Margin.Top,
+                         self.Page.Width - self.Page.Margin.Left - self.Page.Margin.Right,
+                         self.Page.Height - self.Page.Margin.Top - self.Page.Margin.Bottom)
 
             for py in range(pg_Num_Rows):
                 for px in range(pg_Num_Cols):
-                    DX = (px * self.Domino_Width) + (px * self.DominoPadding) + self.Page.Margin.Left + x_offset
-                    DY = (py * self.RowSpacing) + (py * self.Domino_Height) + self.Page.Margin.Bottom + y_offset
-                    
-                    if (ValueIndex == ValueIndexMax):
+                    DX = self.Page.Margin.Left + x_offset + (px * self.Domino_Width) + (px * self.DominoPadding)
+                    DY = self.Page.Margin.Top + y_offset + (py * self.RowSpacing) + (py * self.Domino_Height)
+
+                    if ValueIndex >= ValueIndexMax:
                         ValueIndex = 0
-                        if (self.Randomize == True): shuffle(Valid_Dominos)
+                        if self.Randomize:
+                            shuffle(Valid_Dominos)
 
-                    self.__place_domino(DX, DY, Valid_Dominos[ValueIndex], pyx_c)
-                    ValueIndex = ValueIndex+1
-
-            pyx_sc = pyx.canvas.canvas()
-            
-            pyx_sc.insert(pyx_c, [pyx.trafo.scale(sx=self.Page.X_ScaleFactor, sy=self.Page.Y_ScaleFactor)]) #Scale Factor Here
-            pyx_page = pyx.document.page(pyx_sc,None, pyx.document.paperformat(self.Page.Width, self.Page.Height),0,0,0,0)
-            pyx_document.append(pyx_page)
-
-        return ()
+                    ValueIndex += 1
+                    self.__place_domino(pdf, DX, DY, Valid_Dominos[ValueIndex])
 
     def SavePDF(self, filepath):
-        pyx.unit.set(defaultunit=self.Units)
-        pyx_document = pyx.document.document()
+        pdf = FPDF(unit='in', format=(self.Page.Width, self.Page.Height))
+        self.__prepare_file(pdf)
+        pdf.output(filepath)
 
-        self.__prepare_file(pyx_document)
-
-        pyx_document.writePDFfile(filepath)
-        del pyx_document
-        return ()
 
 if __name__ == "__main__":
-    print ("Domino Printed Fidicual Generator for use with Shaper Origin")
-    print ("The default settings below will a US letter sized document consisting of all valid dominos in order.")
-    print ("Values start over at page 12.")
-    print ("Currently using a domino list of {} values." . format(len(Valid_Dominos)))
-
-
-    testdoc = pyDominoPDF()
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+    testdoc = DominoPDF()
     testdoc.Units = "inch"
     testdoc.Page.Height = 11
     testdoc.Page.Width = 8.5
-
     testdoc.Page.Center_Vertical = True
     testdoc.Page.Center_Horizontal = True
-
     testdoc.Page.Margin.Top = 0.6
     testdoc.Page.Margin.Left = 0.6
     testdoc.Page.Margin.Right = 0.6
     testdoc.Page.Margin.Bottom = 0.6
-    
-    testdoc.Page.Count = 12
-
-    #Page Scaling, 1 = 100%, unitless
-    testdoc.Page.X_ScaleFactor = 1.0
-    testdoc.Page.Y_ScaleFactor = 1.0
-
-
-    #Vertical spacing of the rows, in document units
+    testdoc.Page.Count = 1
     testdoc.RowSpacing = 0.5
-
-    #Randomize Domino Order (Or Not)
-    testdoc.Randomize = False
-
-    #Radius Corners of Domino
+    testdoc.Randomize = True
     testdoc.RadiusCorners = True
-
-    #Margin Border, Useful for debugging the printable area
+    testdoc.PrintValues = True
     testdoc.MarginBorder = False
-
-    #Save PDF, YYYYMMDD-HHMM
-    date_time = datetime.now().strftime("%Y%m%d-%H%M")
-    filename = ("Output-{}.pdf" . format(date_time))	 
+    filename = f"Output-{datetime.now().strftime('%Y%m%d-%H%M')}.pdf"
     testdoc.SavePDF(filename)
     print('Done.')
-
-
-
-
-
-
-
--- pyDominoValueGenerator.py
+++ pyDominoValueGenerator.py
@@ -4,71 +4,133 @@ __copyright__ = "Copyright (C) 2020, Dev@BernCodes.com - Released under terms of
 from bitstring import BitArray, BitStream
 import logging
 from datetime import datetime
+import sys
 
-Valid_Dominos = [63, 95, 111, 119, 123, 125, 126, 159, 175, 183, 187, 189, 190, 207, 215, 219, 221, 222, 231, 235, 237, 238, 243, 245, 246, 249, 250, 252, 287, 303, 311, 315, 317, 318, 335, 343, 347, 349, 350, 359, 363, 365, 366, 371, 373, 374, 377, 378, 380, 399, 407, 411, 413, 414, 423, 427, 429, 430, 435, 437, 438, 441, 442, 444, 455, 459, 461, 462, 467, 469, 470, 473, 474, 476, 483, 485, 486, 489, 490, 492, 497, 498, 500, 543, 559, 567, 571, 573, 574, 591, 599, 603, 605, 606, 615, 619, 621, 622, 627, 629, 630, 633, 634, 636, 655, 663, 667, 669, 670, 679, 683, 685, 686, 691, 693, 694, 697, 698, 700, 711, 715, 717, 718, 723, 725, 726, 729, 730, 732, 739, 741, 742, 745, 746, 748, 753, 754, 783, 791, 795, 797, 798, 807, 811, 813, 814, 819, 821, 822, 825, 826, 828, 839, 843, 845, 846, 851, 853, 854, 857, 858, 860, 867, 869, 870, 873, 874, 881, 882, 903, 907, 909, 910, 915, 917, 918, 921, 922, 931, 933, 934, 937, 938, 945, 946, 963, 965, 966, 969, 970, 977, 978, 993, 994, 1055, 1071, 1079, 1083, 1085, 1086, 1103, 1111, 1115, 1117, 1118, 1127, 1131, 1133, 1134, 1139, 1141, 1142, 1145, 1146, 1167, 1175, 1179, 1181, 1182, 1191, 1195, 1197, 1198, 1203, 1205, 1206, 1209, 1210, 1223, 1227, 1229, 1230, 1235, 1237, 1238, 1241, 1242, 1251, 1253, 1254, 1257, 1258, 1265, 1295, 1303, 1307, 1309, 1310, 1319, 1323, 1325, 1326, 1331, 1333, 1334, 1337, 1338, 1351, 1355, 1357, 1358, 1363, 1365, 1366, 1369, 1370, 1379, 1381, 1382, 1385, 1393, 1415, 1419, 1421, 1422, 1427, 1429, 1430, 1433, 1443, 1445, 1446, 1449, 1457, 1475, 1477, 1478, 1481, 1489, 1505, 1551, 1559, 1563, 1565, 1566, 1575, 1579, 1581, 1582, 1587, 1589, 1590, 1593, 1607, 1611, 1613, 1614, 1619, 1621, 1622, 1625, 1635, 1637, 1641, 1649, 1671, 1675, 1677, 1678, 1683, 1685, 1689, 1699, 1701, 1705, 1713, 1731, 1733, 1737, 1745, 1761, 1799, 1803, 1805, 1811, 1813, 1817, 1827, 1829, 1833, 1841, 1859, 1861, 1865, 1873, 1889, 1923, 1925, 1929, 1937, 1953, 1985, 2079, 2095, 2103, 2107, 2109, 2127, 2135, 2139, 2141, 2151, 2155, 2157, 2163, 2165, 2169, 2191, 2199, 2203, 2205, 2215, 2219, 2221, 2227, 2229, 2233, 2247, 2251, 2253, 2259, 2261, 2265, 2275, 2277, 2281, 2319, 2327, 2331, 2333, 2343, 2347, 2349, 2355, 2357, 2361, 2375, 2379, 2381, 2387, 2389, 2393, 2403, 2405, 2439, 2443, 2445, 2451, 2453, 2467, 2469, 2499, 2501, 2575, 2583, 2587, 2589, 2599, 2603, 2605, 2611, 2613, 2631, 2635, 2637, 2643, 2645, 2659, 2695, 2699, 2701, 2707, 2723, 2755, 2823, 2827, 2835, 2851, 2883, 2947, 3087, 3095, 3099, 3111, 3115, 3123, 3143, 3147, 3155, 3207, 3211, 3335]
+Valid_Dominos = [
+    63, 95, 111, 119, 123, 125, 126, 159, 175, 183,
+    187, 189, 190, 207, 215, 219, 221, 222, 231, 235,
+    237, 238, 243, 245, 246, 249, 250, 252, 287, 303,
+    311, 315, 317, 318, 335, 343, 347, 349, 350, 359,
+    363, 365, 366, 371, 373, 374, 377, 378, 380, 399,
+    407, 411, 413, 414, 423, 427, 429, 430, 435, 437,
+    438, 441, 442, 444, 455, 459, 461, 462, 467, 469,
+    470, 473, 474, 476, 483, 485, 486, 489, 490, 492,
+    497, 498, 500, 543, 559, 567, 571, 573, 574, 591,
+    599, 603, 605, 606, 615, 619, 621, 622, 627, 629,
+    630, 633, 634, 636, 655, 663, 667, 669, 670, 679,
+    683, 685, 686, 691, 693, 694, 697, 698, 700, 711,
+    715, 717, 718, 723, 725, 726, 729, 730, 732, 739,
+    741, 742, 745, 746, 748, 753, 754, 783, 791, 795,
+    797, 798, 807, 811, 813, 814, 819, 821, 822, 825,
+    826, 828, 839, 843, 845, 846, 851, 853, 854, 857,
+    858, 860, 867, 869, 870, 873, 874, 881, 882, 903,
+    907, 909, 910, 915, 917, 918, 921, 922, 931, 933,
+    934, 937, 938, 945, 946, 963, 965, 966, 969, 970,
+    977, 978, 993, 994, 1055, 1071, 1079, 1083, 1085, 1086,
+    1103, 1111, 1115, 1117, 1118, 1127, 1131, 1133, 1134, 1139,
+    1141, 1142, 1145, 1146, 1167, 1175, 1179, 1181, 1182, 1191,
+    1195, 1197, 1198, 1203, 1205, 1206, 1209, 1210, 1223, 1227,
+    1229, 1230, 1235, 1237, 1238, 1241, 1242, 1251, 1253, 1254,
+    1257, 1258, 1265, 1295, 1303, 1307, 1309, 1310, 1319, 1323,
+    1325, 1326, 1331, 1333, 1334, 1337, 1338, 1351, 1355, 1357,
+    1358, 1363, 1365, 1366, 1369, 1370, 1379, 1381, 1382, 1385,
+    1393, 1415, 1419, 1421, 1422, 1427, 1429, 1430, 1433, 1443,
+    1445, 1446, 1449, 1457, 1475, 1477, 1478, 1481, 1489, 1505,
+    1551, 1559, 1563, 1565, 1566, 1575, 1579, 1581, 1582, 1587,
+    1589, 1590, 1593, 1607, 1611, 1613, 1614, 1619, 1621, 1622,
+    1625, 1635, 1637, 1641, 1649, 1671, 1675, 1677, 1678, 1683,
+    1685, 1689, 1699, 1701, 1705, 1713, 1731, 1733, 1737, 1745,
+    1761, 1799, 1803, 1805, 1811, 1813, 1817, 1827, 1829, 1833,
+    1841, 1859, 1861, 1865, 1873, 1889, 1923, 1925, 1929, 1937,
+    1953, 1985, 2079, 2095, 2103, 2107, 2109, 2127, 2135, 2139,
+    2141, 2151, 2155, 2157, 2163, 2165, 2169, 2191, 2199, 2203,
+    2205, 2215, 2219, 2221, 2227, 2229, 2233, 2247, 2251, 2253,
+    2259, 2261, 2265, 2275, 2277, 2281, 2319, 2327, 2331, 2333,
+    2343, 2347, 2349, 2355, 2357, 2361, 2375, 2379, 2381, 2387,
+    2389, 2393, 2403, 2405, 2439, 2443, 2445, 2451, 2453, 2467,
+    2469, 2499, 2501, 2575, 2583, 2587, 2589, 2599, 2603, 2605,
+    2611, 2613, 2631, 2635, 2637, 2643, 2645, 2659, 2695, 2699,
+    2701, 2707, 2723, 2755, 2823, 2827, 2835, 2851, 2883, 2947,
+    3087, 3095, 3099, 3111, 3115, 3123, 3143, 3147, 3155, 3207,
+    3211, 3335
+]
 
 
-def GenerateValidDominos(Start = 0, End = 4095):
-    logging.debug("Checking Domino Values from {} to {}" . format(Start, End))
+#  A domino is defined:
+#    - two rows
+#    - eight columns / the first and last column is always set
+#
+#    - when 2827 => 101100000111 the domino is
+#
+#       11011001
+#       10001111
+#
+#    - when 0663 => 001010010111 the domino is
+#
+#       10010101
+#       10101111
+#
+# The bitstring 001010010111 is divided in the middle (001010 010111). The first section will be the first row,
+# the second part is the second row. This only works with max. 12 bits -> this will give a maximum of 452 dominos, when
+# the follwing rules for valid numbers are applied:
+#
+#   - the numbers binary value must have a Hamming weight of 6 -> this garantees that at least 6 positions in the domino are white dots
+#   - the two parts of the bitstring must not be mirrored such as 011011 110110
+#   - a reversed bitstring must not be seen before
 
-    bPass = 1
-    Row1 = 0
-    Row2 = 0
+def GenerateValidDominos(Start=0, End=4095):
+    logging.debug("Checking Domino Values from {} to {}".format(Start, End))
+
     ValidValues = []
 
-    for x in range(Start,(End+1),1):
+    for x in range(Start, (End + 1), 1):
         bPass = True
-        DPips = BitArray('0x000')
-        DPips.uint = x
+        DPips = BitArray(uint=x, length=12)
         Row1 = DPips[:6]
         Row2 = DPips[-6:]
-        logging.debug("Index Value:{} Binary Value:{}" .format(x,DPips.bin))
-        
-        #Row 1 and 2 Pip Count
+        logging.debug("Index Value:{} Binary Value:{}".format(x, DPips.bin))
+
+        # Row 1 and 2 Pip Count
         R1_PC = len(list(Row1.findall([1])))
         R2_PC = len(list(Row2.findall([1])))
 
-        #Total pips between rows must be 6.
+        # Total pips between rows must be 6.
         if (R1_PC + R2_PC) != 6: bPass = False
 
-        logging.debug("Row 1 Value:{} Binary Value:{} Pip Count:{}" .format(Row1.uint, Row1.bin, R1_PC))
-        logging.debug("Row 2 Value:{} Binary Value:{} Pip Count:{}" .format(Row2.uint, Row2.bin, R2_PC))
-        logging.debug("Pip count validity: {}" . format(bPass))
+        logging.debug("Row 1 Value:{} Binary Value:{} Pip Count:{}".format(Row1.uint, Row1.bin, R1_PC))
+        logging.debug("Row 2 Value:{} Binary Value:{} Pip Count:{}".format(Row2.uint, Row2.bin, R2_PC))
+        logging.debug("Pip count validity: {}".format(bPass))
 
-        #Generate the reverse of the bit stream.
-        DFlip = BitArray('0x000')
-        DFlip.uint = x
+        # Generate the reverse of the bit stream.
+        DFlip = BitArray(uint=x, length=12)
         DFlip.reverse()
         ReversedValue = DFlip.uint
 
-        logging.debug("Flipped Row Value:{} Binary Value:{}" .format(ReversedValue, DFlip.bin))        
+        logging.debug("Flipped Row Value:{} Binary Value:{}".format(ReversedValue, DFlip.bin))
 
-        #The domino is a mirror iamge of itself and is invalid.
-        if (ReversedValue == x):
+        # The domino is a mirror image of itself and is invalid.
+        if ReversedValue == x:
             logging.debug("Fail: Mirror Domino")
             bPass = False
-        
-        #The rotated domino exists as a prior value.
-        if (ReversedValue in ValidValues):
+
+        # The rotated domino exists as a prior value.
+        if ReversedValue in ValidValues:
             logging.debug("Fail: Exists previously (rotated)")
             bPass = False
 
-        #The specified domino passed all rules and is valid.
-        if (bPass == True):
+        # The specified domino passed all rules and is valid.
+        if bPass:
             ValidValues.append(x)
-            logging.debug("Passed: Added Value {}({}) to list." . format(x, DPips.bin))
-
+            logging.debug("Passed: Added Value {}({}) to list.".format(x, DPips.bin))
 
         logging.debug("---")
 
     logging.debug(ValidValues)
     return ValidValues
 
+
 if __name__ == "__main__":
     date_time = datetime.now().strftime("%Y%m%d-%H%M")
-    logfile = ("Domino_Log_{}.log" . format(date_time))
-    logging.basicConfig(filename=logfile,level=logging.DEBUG)
-    GeneratedDominos = GenerateValidDominos(0,4095)
-    print ("Found a total of {} dominos" . format(len(GeneratedDominos)))
-
-
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+    GeneratedDominos = GenerateValidDominos(0, 4095)
+    print("Found a total of {} dominos".format(len(GeneratedDominos)))
